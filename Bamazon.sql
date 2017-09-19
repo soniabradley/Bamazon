24649f10f886b4979b3177bfaf09954c435ddcb2
soniab <sonia.bradley524@gmail.com>
2017-09-19 18:04:03 -0500

    added subtotals

---
+++ Bamazon.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS Bamazon;
+CREATE database Bamazon;
+
+USE Bamazon;
+
+CREATE TABLE Products (
+    ItemID INTEGER AUTO_INCREMENT NOT NULL,
+    ProductName VARCHAR(50) NOT NULL,
+    DepartmentName VARCHAR(50) NOT NULL,
+    Price DECIMAL(10,2) NOT NULL,
+    StockQuantity INTEGER(10) NOT NULL,
+    PRIMARY KEY (ItemID)
+);
+
+
+
+INSERT INTO Products (ProductName, DepartmentName, Price, StockQuantity)
+VALUES ('Lavender Cloud', 'Paint', 49.99, 50),
+        ('Sweet Violet', 'Paint', 39.99, 12),
+        ('Siberian Iris', 'Paint', 59.99, 48),
+        ('Petasites Japonicus', 'Plants', 9.99, 215),
+        ('Astilboides Tabularis', 'Plants', 12.49, 116),
+        ('Rheum Palmatum', 'Plants', 8.00, 10),
+        ('Fruit-and-Nut Cookies', 'Food', 3.49, 73),
+        ('Chocolate Monkey Bars', 'Food', 5.00, 112),
+        ('Orange-Almond Bars', 'Food', 2.99, 60),
+        ('Diamond Ring', 'Jewelry', 2400.00, 1);
+
+SELECT * FROM  Products;
+
+UPDATE 
+INSERT
---
+++ bamazon2.js
@@ -0,0 +1,108 @@
+// node modules (npm)
+var mysql = require("mysql");
+var Table = require("cli-table");
+var inquirer = require("inquirer");
+
+// sql connection
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "MyNewPass",
+  database: "Bamazon"
+})
+
+// new cli-table instantiate
+var table = new Table({
+  head: ['ItemID', 'ProductName', 'DepartmentName', 'Price', 'StockQuantity']
+
+})
+
+// initialize connection
+connection.connect(function (err) {
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  start();
+})
+// function to print all items to the console, uses npm module cli-table
+var start = function () {
+  // get all rows from the products table
+  connection.query("SELECT * FROM Products", function (err, res) {
+    if (err) throw err;
+    // add all of the rows to the cli-table
+    for (var i = 0; i < res.length; i++) {
+      // table is Array, so you can push
+      console.log([res[i].ItemID, res[i].ProductName, res[i].DepartmentName, res[i].stock, res[i].StockQuantity]);
+    }
+    promptCustomer(res);
+  })
+}
+//1. Ask user question (inquirer)
+//2. If necessary, access database depending on user answer to question (mysql db)
+//3. Once you get data from db, display on terminal using table (cli-table)
+var promptCustomer = function (res) {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'choice',
+      message: "What would you like to purchase? [Quit with Q]"
+    }])
+
+  .then(function (answer) {
+      var correct = false;
+      // type in item name and it will loop through the response
+      for (var i = 0; i < res.length; i++) {
+        if (res[i].ProductName === answer.choice) {
+          // if product is in the string = true
+          correct = true;
+          // it then makes the product choice
+          var product = answer.choice;
+          // set's id to the product selected
+          var id = i;
+
+      // how many of those items they would like to buy
+    inquirer.prompt({
+          type: "input",
+          name: "quantity",
+          message: "How many would you like to buy?",
+          validate: function (value) {
+              if (isNaN(value) === false) {
+                return true;
+              } else {
+                return false;
+              }
+            }
+          })
+    .then(function (answer) {
+              if ((res[id].StockQuantity - answer.quantity) > 0) {
+                connection.query('UPDATE Products SET StockQuantity = StockQuantity - ? WHERE ProductName = ?',
+                  [answer.quantity, product],
+                  function (err, res2) {
+                    console.log("Product Bought");
+                    start();
+                  }
+                );
+              } else {
+                console.log("Not a valid selection");
+                promptCustomer(res);
+              }
+            })
+      var promptCustomer = function (res) {
+              inquirer.prompt([
+                {
+                  type: 'input',
+                  name: 'choice',
+                  message: "What would you like to purchase? [Quit with Q]"
+                }])
+
+
+        }
+      }
+      if (i === res.length && correct === false) {
+        console.log("Not a valid selection");
+        promptCustomer(res);
+      }
+
+
+    })
+}
